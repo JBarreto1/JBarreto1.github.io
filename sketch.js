b58f366c222caca3287bb43df568258f8e1ceb4d
Jonah Barreto <barretojonah@gmail.com>
2021-05-03 22:21:17 -0500

    ball catches on the ratchet

--- sketch.js
+++ sketch.js
@@ -1,29 +1,45 @@
 let ballLoc = 0 //which step is the ball on?
-let ballOnReg = true //which stair set is the ball on? 1=reg 0=flashing
-let regWidth = 60
-let flashWidth = 80
-let regSpeed = 2
-let flSpeed1 = 4
-let flSpeed2 = -2
+let ballOnReg = true //which stair set is the ball on? true=reg false=flashing
+let regWidth = 60 //control the width of the regular step in pixels
+let flashWidth = 80 //control the width of the flash step in pixels
+
+let regSpeed = 2 //one speed for regular and 2 for the flashing
+let flSpeed1 = 4 //the fast flashing DOWN speed
+let flSpeed2 = -2 //slow flashing UP speed
+
+let showReg = true
+let showFlashing = true
+
 let regular = []
 let flashing = []
 
+
 function setup() {
 	createCanvas(600,500);
-	for (let i = -2; i < 10; i++) {
-		let x = 60 * i
-		let r = new Stairs(x,40,regWidth,120,regSpeed)
-		regular.push(r)
+	//construct the stairs for both ratchets
+	//each ratchet is made of 12 steps that begin of screen (negative x value)
+	if (showReg) {
+		for (let i = -2; i < 10; i++) {
+			let x = 60 * i
+			let r = new Stairs(x,40,regWidth,120,regSpeed)
+			regular.push(r)
+		}
+	} else {
+		ballOnReg = false //if the regular stair is off, the ball has to start on flashing
 	}
-	for (let i = -2; i < 10; i++) {
-		let x = 60 * i
-		let r = new Stairs(x,40,flashWidth,0,flSpeed1)
-		flashing.push(r)
+	if (showFlashing){
+		for (let i = -2; i < 10; i++) {
+			let x = 60 * i
+			let r = new Stairs(x,40,flashWidth,0,flSpeed1)
+			flashing.push(r)
+		}
 	}
+	
 }
 
 function draw() {
 	background(0);
+	//move and draw all the steps for both flashing and regular stairs EXCEPT the one that has the ball
 	for (let i = 0; i < flashing.length; i++) {
 		flashing[i].move(flSpeed1,flSpeed2)
 		if (i != ballLoc || ballOnReg) {
@@ -35,26 +51,32 @@ function draw() {
 		if (i != ballLoc || !ballOnReg) {
 			regular[i].show()
 		}
-	}
+	}	
 	let curBallLoc = ballLoc
 	if (ballOnReg) {
-		regular[curBallLoc].show(true)
 		for (let i = 0; i < flashing.length; i++) {
-			// console.log(flashing[i].x)
-			// console.log(regular[ballLoc].x)
-			if (flashing[i].x == regular[curBallLoc].x && regular[curBallLoc].currentSpeed > flashing[i].currentSpeed) {
-				ballLoc = i
-				ballOnReg = false
+			//if the ball is on the regular stair cycle through the flashing steps
+			//if one of the flashing steps is between where the ball is now and where its going to be LOWER then it catches the ball
+			if (flashing[i].x <= regular[curBallLoc].x + regular[curBallLoc].currentSpeed && flashing[i].x >= regular[curBallLoc].x) {
+				if (regular[curBallLoc].currentSpeed > flashing[i].currentSpeed) {
+					ballLoc = i
+					ballOnReg = false
+				}
 			}
 		}
+		regular[curBallLoc].show(true)
 	} else {
-		flashing[curBallLoc].show(true)
 		for (let i = 0; i < regular.length; i++) {
-			if (regular[i].x == flashing[curBallLoc].x && (flashing[curBallLoc].currentSpeed > regular[i].currentSpeed)) {
-				ballLoc = i
-				ballOnReg = true
+			//if the ball is on the regular stair cycle through the flashing steps
+			//if one of the flashing steps is between where the ball is now and where its going to be HIGHER then it catches the ball
+			if (regular[i].x > flashing[curBallLoc].x - flashing[curBallLoc].currentSpeed && regular[i].x < flashing[curBallLoc].x) {
+				if(flashing[curBallLoc].currentSpeed > regular[i].currentSpeed) {
+					ballLoc = i
+					ballOnReg = true
+				}
 			}
 		}
+		flashing[curBallLoc].show(true)
 	}
 }
 
@@ -68,12 +90,15 @@ class Stairs {
 		this.currentSpeed = speed
 	}
 	move(speedOne,speedTwo) {
+		//move the stairs down. if we reach the bottom (we went beyond the 600 canvas) jump to a bit beyond the top
 		if (this.x > 600) {
 			this.x = -120
 		} else {
+			//if there is only one speed, then it's the regualr stairs and just move at that speed
 			if (typeof speedTwo === 'undefined') {
 				this.x = this.x + speedOne
 			} else {
+				//cycle every 50 between fast down and slow up speed
 				if (Math.ceil(this.counter / 50) % 2 === 1 ) {
 					this.x = this.x + speedOne
 					this.currentSpeed = speedOne
@@ -101,25 +126,4 @@ class Stairs {
 			ellipse(newy + 50, newx - 15, 30);
 		}
 	}
-
-	// show() {
-	// 	stroke(255)
-	// 	strokeWeight(1);
-	// 	let newx = this.x - 1000
-	// 	let newy = this.y
-	// 	fill(this.color)
-	// 	beginShape();
-	// 	vertex(newy + 20, newx);
-	// 	for (var i = 0; i < 200; i++) {
-	// 		if (i % 2 === 0){
-	// 			vertex(newy + 60, newx)
-	// 		} else {
-	// 			vertex(newy + 60 + this.stepWidth, newx)
-	// 			newx = newx + 50
-	// 		}
-	// 	}
-	// 	vertex(newy + 20, newx);
-	// 	endShape(CLOSE);
-	// 	return this.x
-	// }
 }
